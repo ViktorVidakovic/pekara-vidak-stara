5be83e9862ce3a2904403bc539b90d853198de8e
ViktorVidakovic <75852233+ViktorVidakovic@users.noreply.github.com>
2021-12-11 22:28:04 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 //Slajder
 var timer;
 var slajdIndeks = 1;
@@ -22,142 +23,86 @@ function showSlides(n) {
 }
 console.log("Provera slajdera");
 
-//Forma -GLEDATI MILENU
-document.querySelector("#btnPosalji").addEventListener("click",provera());
-
+//Forma - provera
 function provera(){
-  var objImePrezime, objAdresa, objGrad, objPosBroj, objPonuda, objRadio, nizUspesno,nizGreske;
-  objImePrezime = document.querySelector("#imePrezime");
-  objAdresa = document.querySelector("#adresa");
-  objGrad = document.querySelector("#grad");
-  objPosBroj = document.querySelector("#posBroj");
-  objPonuda = document.querySelector("#ponuda");
-  objRadio = document.getElementsByName("radio1");
-  nizUspesno = [];
-  nizGreske = [];
+  var imePrezime;
+  var vrImePrezime;
+  imePrezime = document.querySelector("#imePrezime");
+  vrImePrezime = imePrezime.value;
 
-  // Provera imePrezime
-  if(objImePrezime.value.length < 4){
-    nizGreske.push("Pogrešno popunjeno polje za ime i prezime.");
-  }
-  else{
-    nizUspesno.push(objImePrezime.value);
+  var adresa;
+  var vrAdresa;
+  adresa = document.querySelector("#adresa");
+  vrAdresa = adresa.value;
+
+  var grad;
+  var vrGrad;
+  grad = document.querySelector("#grad");
+  vrGrad = grad.value;
+
+  var posBroj;
+  var vrPosBroj;
+  posBroj = document.querySelector("#posBroj");
+  vrPosBroj = posBroj.value;
+
+  var ponuda;
+  ponuda = document.querySelector("#ponuda");
+
+  var nacinPreuzimanja
+  nacinPreuzimanja=document.getElementsByName("nacinPreuzimanja");
+
+  //Proveri ime i prezime
+  let regImePrezime=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}$/;
+  if(!(regImePrezime.test(vrImePrezime))){
+    document.querySelector("#poljeImePrezime > p").innerHTML = "Pogrešno uneto ime i prezime";
+    document.querySelector("#poljeImePrezime > p").style.color = "white";
+    document.querySelector("#poljeImePrezime > p").style.fontSize = "14px";
   }
 
-  // Provera adresa
-  if(objAdresa.value.length < 4){
-    nizGreske.push("Pogrešno popunjeno polje za ime i prezime.");
-  }
-  else{
-    nizUspesno.push(objImePrezime.value);
+  //Proveri adresu
+  let regAdresa=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[0-9]{1,4}$/;
+  let regAdresa2=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[0-9]{1,4}$/;
+  if(!(regAdresa.test(vrAdresa)) || !(regAdresa2.test(vrAdresa))){
+    document.querySelector("#poljeAdresa > p").innerHTML = "Pogrešno uneta adresa";
+    document.querySelector("#poljeAdresa > p").style.color = "white";
+    document.querySelector("#poljeAdresa > p").style.fontSize = "14px";
   }
 
+  //Proveri grad
+  let regGrad=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}$/;
+  let regGrad2=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[A-ZČĆŠĐŽ]{0,1}[a-zčćšđž]{2,15}$/;
+  if(!(regGrad.test(vrGrad)) || !(regGrad2.test(vrGrad))){
+    document.querySelector("#poljeGrad > p").innerHTML = "Pogrešno uneta opština";
+    document.querySelector("#poljeGrad > p").style.color = "white";
+    document.querySelector("#poljeGrad > p").style.fontSize = "14px";
+  }
+
+  //Proveri postanski broj
+  let regPosBroj=/^[0-9]{5}$/;
+  if(!(regPosBroj.test(vrPosBroj))){
+    document.querySelector("#poljePosBroj > p").innerHTML = "Pogrešno unet poš. broj";
+    document.querySelector("#poljePosBroj > p").style.color = "white";
+    document.querySelector("#poljePosBroj > p").style.fontSize = "14px";
+  }
+
+  //Provera padajuce 
+  if(ponuda.options[ponuda.options.selectedIndex].value=="0"){
+    document.querySelector("#izbor > p").innerHTML="Morate izabrati proizvod";
+    document.querySelector("#izbor > p").style.color="white";
+    document.querySelector("#izbor > p").style.fontSize = "14px";
+  }
+
+  //Provera radio button-a
+  var vrNacinPreuzimanja=" ";
+  for(let i=0;i<nacinPreuzimanja.length;i++){
+    if(nacinPreuzimanja[i].checked){
+      vrNacinPreuzimanja=nacinPreuzimanja[i].value;
+      break;
+    }
+  }
+  if(vrNacinPreuzimanja==" "){
+    document.querySelector("#nacinPreuzimanja > div > p").innerHTML="Morate izabrati nacin isporuke";
+    document.querySelector("#nacinPreuzimanja > div > p").style.color="white";
+    document.querySelector("#nacinPreuzimanja > div > p").style.fontSize = "14px";
+  }
 }
-//Obrada forme
-// function proveri(){
-//   var imePrezime;
-//   var vrImePrezime;
-//   imePrezime = document.querySelector("#imePrezime");
-//   vrImePrezime = imePrezime.value;
-//   //Proveri ime i prezime
-//   let regImePrezime=/^[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}\s[A-ZČĆŠĐŽ]{1}[a-zčćšđž]{2,15}$/;
-//   if(regImePrezime.test(vrImePrezime)){
-//     document.querySelector("#polje-ime-prezime > p").innerHTML = "Uspešno ste uneli ime i prezime";
-//     document.querySelector("#polje-ime-prezime > p").style.color = "green";
-//   }
-//   else{
-//     document.querySelector("#polje-ime-prezime > p").innerHTML = "Pogrešno uneto ime i prezime";
-//     document.querySelector("#polje-ime-prezime > p").style.color = "red";
-//   }
-// }
-
-
-
-
-
-// //forma
-// let padajuci = id("padajuca_lista");
-
-// var izbor(opcije) = [
-//   "Izaberite...",
-//   "Krofne",
-//   "Mini-pice",
-//   "Kiflice sa viršlama",
-//   "Hleb",
-//   "Margarita",
-//   "Capriciosa",
-//   "Quattro Stagioni",
-//   "Vegeteriana",
-// ];
-// var vr(vrednosti) = [
-//   "0",
-//   "krofne",
-//   "mini",
-//   "kiflice",
-//   "hleb",
-//   "margarita",
-//   "capriciosa",
-//   "quattro",
-//   "vegeteriana",
-// ];
-
-// let li = document.createElement("select");
-// li.setAttribute("name", "problem");
-// li.setAttribute("id", "problem");
-// li.setAttribute("required", "required");
-
-// for (let i = 0; i < izbor.length; i++) {
-//   var noviIzbor = document.createElement("option");
-//   noviIzbor.setAttribute("value", vr[i]);
-//   noviIzbor.setAttribute("name", "izbor");
-//   noviIzbor.innerHTML = izbor[i];
-//   li.appendChild(noviIzbor);
-// }
-// let padajucaLista = id("padajuca_lista");
-// padajucaLista.appendChild(lista);
-
-// //provera forme
-// let form = id("form"),
-//   ime = id("name"),
-//   email = id("email"),
-//   telefon = id("tel"),
-//   termin = id("termin"),
-//   usluga = document.getElementsByName("izbor"), successIcon = classes("success-icon"),
-//   failureIcon = classes("failure-icon");
-//   greska = classes("greska");
-// var brojGresaka;
-// const regIme=/^[A-ZČĆŽŠĐ][a-zčćžšđ]{2,15}$/,regEmail= /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-// form.addEventListener("submit", (e) => {
-//   brojGresaka = 0;
-//   e.preventDefault();
-//   engine(ime, 0, "Ime i Prezime ne mogu biti prazni!");
-//   engine(email, 1, "Email ne može biti prazan!");
-//   engine(telefon, 2, "Telefon ne može biti prazan!");
-//   engine(termin, 3, "Termin ne može biti prazan!");
-//   if (brojGresaka == 0) {alert("Forma uspešno odrađena");  }
-// });
-// form.addEventListener("reset", (e) => {
-//   e.preventDefault();
- 
-//   for(let i=0;i<4;i++){
-//     reset(i);
-//   }
-// });
-// let reset=(serial)=>{
-//   failureIcon[serial].classList.add("prazno")
-//   successIcon[serial].classList.add("prazno");
-//   greska[serial].innerHTML = "";
-// }
-// let engine = (id, serial, poruka) => {
-//   if (id.value.trim() == "") {
-//     greska[serial].innerHTML = poruka;
-//     failureIcon[serial].classList.remove("prazno")
-//     successIcon[serial].classList.add("prazno");
-//     brojGresaka++;
-
-//   } else {
-//     greska[serial].innerHTML = "";
-//     failureIcon[serial].classList.add("prazno")
-//     successIcon[serial].classList.remove("prazno");
-
-//   }
